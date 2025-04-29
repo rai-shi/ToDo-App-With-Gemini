70c0bdb7125943cdb65ce50af68d696ccbff761a
Aysenur <aysenurtak1@gmail.com>
2025-04-29 22:05:46 +0300

    database connection is done

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI 
 
+from models import Base 
+from database import engine
 
 app = FastAPI()
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
--- models.py
+++ models.py
@@ -1,1 +1,16 @@
 # ORM
+
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+
+
+class ToDo(Base):
+    __tablename__ = "todos"
+
+    id          = Column(Integer, primary_key=True, index=True)
+    title       = Column(String, index=True)
+    description = Column(String)
+    priority    = Column(Integer, default=1) # would be in range 1-5 
+    completed   = Column(Boolean, default=False) 
+    created_at  = Column(DateTime)
+    updated_at  = Column(DateTime)
